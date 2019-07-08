560b6ea2e9bf826324ee224faca972710dc710cd
Sam Yakovlev <samuel.yakovlev@gmail.com>
2019-07-08 11:16:43 -0400

    Placed utility methods into Utils file

--- AIS_Path_Utils.py
+++ AIS_Path_Utils.py
@@ -9,6 +9,8 @@ speedy = [40, 41, 42, 43, 44, 49]
 cargo = [70, 71, 72, 73, 74, 79, 1003, 1004, 1016]
 tug = [31, 32, 52, 1025]
 
+### Return vessel types as a string based on code in AIS data
+#	This is pretty self-explanatory
 def resolve_vessel_type(vessel_code):
 	try:
 		vType = int(vessel_code)
@@ -32,3 +34,22 @@ def resolve_vessel_type(vessel_code):
 		return("OSV")
 	else:
 		return("Unknown") # if nothing is found, again return failure
+
+### Plot tracks of vessels, for visualization purposes and understanding it with human eyes
+#	Every graph is also labeled with its REPORTED vessel type
+def plot_vessel_tracks(paths):
+	path_iterator = iter(paths)
+	start = 300
+	for i in range(start):
+		current_ship_course = next(path_iterator) # skip to that index
+	for i in range(start, start+100):
+		current_ship_course = next(path_iterator)
+		values = paths[current_ship_course].values
+		plt.subplot(10, 10, i-start+1)
+		plt.axis('off')
+		plt.plot(values[:, 1], values[:, 2])
+		if (np.isnan(values[0, 6]) == True):
+			plt.title(ais_utils.resolve_vessel_type(values[0, 6])) 
+		else:
+			plt.title(str(int(values[0, 6])) + ": " + ais_utils.resolve_vessel_type(values[0, 6]))
+	plt.show() # Show off the first 25 ship tracks
