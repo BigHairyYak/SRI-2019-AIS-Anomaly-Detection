e0624ca60343aff8eb5b79c7fc88ecf2220f2817
Eamon <eamonbontempo@aol.com>
2019-07-16 11:35:59 -0400

    Organized code for better readability and modularity

--- AIS_RNN.py
+++ AIS_RNN.py
@@ -1,39 +1,43 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import math
 from sklearn.preprocessing import MinMaxScaler
+
 
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 
 
-#import data for each vessel
+
 
 ###build model
 model = Sequential()
-model.add(LSTM(units=30, return_sequences= True, input_shape=(None,4)))
-model.add(LSTM(units=30, return_sequences=True))
-model.add(LSTM(units=30))
+model.add(LSTM(units=100, return_sequences= True, input_shape=(None,4)))
+#model.add(LSTM(units=30, return_sequences=True))
+model.add(LSTM(units=100))
 model.add(Dense(units=4))
 model.compile(optimizer='adam', loss='mean_squared_error', metrics = ['accuracy'])
 
-
+#import data
 data = pd.read_csv("/Users/Eamon/Desktop/cargo.csv", header=0, index_col=0) 
 mmsi_groups = data.groupby(data.index)     #groups data into individual tracks
 
 count = 0
 testPath = None
 
-###loop through each track
+trainX = []
+trainY = []
+testX = []
+testY = []
+
+###loop through each track and assign data to train and test groups
 for MMSI in mmsi_groups.groups:
-    print("hey: " +  str(count))
-    count+=1
     
-    if(count == 51):     #train on first x-1 tracks, use xth for testing
-        testPath = path = data[data.index == MMSI].sort_values(by="BaseDateTime").reset_index(drop=True)
-        break
+    print(len(mmsi_groups.groups))
+    
+    print("Vessel #" +  str(count))
+    count+=1
         
     path = data[data.index == MMSI].sort_values(by="BaseDateTime").reset_index(drop=True) 
     
@@ -45,22 +49,17 @@ for MMSI in mmsi_groups.groups:
     plt.plot(input_feature[:,0], input_feature[:,1])
     plt.xlabel("Latitude")
     plt.ylabel("Longitude")
-    plt.show()
-    
-    ###normalize data
-    sc= MinMaxScaler(feature_range=(0,1))
-    input_data[:,0:4] = sc.fit_transform(input_feature[:,:])
-    
+    plt.show() 
     
     ###choose input and output train data
-    lookback = 100     #the number of past data points used for prediction
-    X=[]               #Train inputs
-    Y=[]               #Train outputs
+    lookback = 30     #the number of past data points used for prediction
+    X=[]               #inputs
+    Y=[]               #outputs
     
     for i in range(len(path)-lookback-1):
         t=[]
         for j in range(0,lookback):
-            t.append(input_data[[(i+j)], :])
+            t.append(input_data[(i+j), :])
         X.append(t)
         Y.append(input_data[i+ lookback, :])
         
@@ -71,73 +70,62 @@ for MMSI in mmsi_groups.groups:
     
     print(X.shape)
     print(Y.shape)
-
-    ###train
+    
+    
+    if(count < 101):    #choose number of training vessels
+        trainX.append(X)
+        trainY.append(Y)
+    elif(count < 201):  #choose number of test vessels
+        testX.append(X)
+        testY.append(Y)
+    else:
+        break
+        
+    
+###Train
+for i in range(len(trainX)):
+    print("Train Vessel #" +  str(i))
+    inputs = trainX[i]
+    outputs = trainY[i]
+    
     try:
-        model.fit(X, Y, epochs=100, batch_size=X.shape[0])
+        model.fit(inputs, outputs, epochs=100, batch_size=inputs.shape[0])
     except:
         print("untrainable: moving on")
         continue
-    #model.reset_states()
-    
-
-###Gather test data
-input_feature = testPath.iloc[:,[1,2,3,4]].values
-input_data = input_feature
-sc= MinMaxScaler(feature_range=(0,1))
-input_data[:,0:4] = sc.fit_transform(input_feature[:,:])
-
-lookback= 100
-
-test_size=int(.3 * len(testPath))
-X_test=[]
-
-for i in range(len(testPath)-lookback-1):
-    t=[]
-    for j in range(0,lookback):
-        t.append(input_data[[(i+j)], :])
-    X_test.append(t)
-
         
-X_test =  np.array(X_test)
-X_test = X_test.reshape(X_test.shape[0],lookback,4)
-X_test = X_test[:len(X_test)//2] #first half of path - this way it predicts second half
-Y_test = input_data[(lookback): X_test.shape[0] + lookback, :]
-Y_test = Y_test.reshape(Y_test.shape[0],4)
-
-###predict
-predictions = model.predict(X_test)
-
-print(X_test.shape)
-print(predictions.shape)
-
-
-
-###plot predictions
-#Plot Path (Lat vs Lon)
-plt.plot(predictions[:,0], predictions[:,1], color= 'red')   
-plt.plot(Y_test[:, 0], Y_test[:,1],  color='green')
-plt.show()
-
-#Plot LAT
-plt.plot(predictions[:,0], color= 'red')
-plt.plot(Y_test[:,0], color='green')
-plt.show()
-
-#Plot LON
-plt.plot(predictions[:,1], color= 'red')
-#plt.plot(input_data[lookback:test_size+(2*lookback), 1 ], color='green')
-plt.plot(Y_test[:,1], color='green')
-plt.show()
-
-#Plot SOG
-plt.plot(predictions[:,2], color= 'red')
-#plt.plot(input_data[lookback:test_size+(2*lookback), 2 ], color='green')
-plt.plot(Y_test[:,2], color='green')
-plt.show()
-
-#Plot COG
-plt.plot(predictions[:,3], color= 'red')
-#plt.plot(input_data[lookback:test_size+(2*lookback), 3 ], color='green')
-plt.plot(Y_test[:,3], color='green')
-plt.show()
+###Predict
+for i in range(len(testX)):
+    print("Test Vessel #" + str(i))
+    inputs = testX[i]
+    outputs = testY[i]
+    
+    predictions = model.predict(inputs)
+    
+    try:     #for unusable tracks (empty inputs)
+        print(predictions.shape)
+    except:
+        continue
+        
+    print(outputs.shape)
+    print(inputs.shape)
+    
+    
+    #Calculate prediction errors
+    for i in range(len(predictions)):
+        LAT_error = abs(predictions[i,0] - outputs[i,0])
+        
+        LON_error = abs(predictions[i,1] - outputs[i,1])
+        
+        SOG_error = abs(predictions[i,2] - outputs[i,2])
+        
+        COG_error = abs(predictions[i,3] - outputs[i,3])
+        
+        if(LAT_error > .03 and LON_error > .03 or SOG_error > .03 or COG_error > .03):  #Error function
+            plt.plot(predictions[i,0], predictions[i,1],'o',markersize=1, color= 'blue')   
+            plt.plot(outputs[i, 0], outputs[i,1], 'o',markersize=1,  color='blue')
+        else:
+            plt.plot(predictions[i,0], predictions[i,1],'o',markersize=1, color= 'red')   
+            plt.plot(outputs[i, 0], outputs[i,1], 'o',markersize=1,  color='green')
+            
+    plt.show()
